7ad3cf1cfbc2edc79c0eec92c0b469295fa5cfa1
yu myeong cheol <mspringmspring@naver.com>
2020-10-13 19:41:38 +0900

    [CREATE] <auth/apikey.ts>

---
+++ src/auth/apikey.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import { ForbiddenError } from '../core/ApiError';
+import { PublicRequest } from 'app-request';
+import asyncHandler from '../middleware/asyncHandler';
+import { apiKey } from '../config';
+import Logger from '../core/Logger';
+import validator, { ValidationSource } from '../middleware/validator';
+import schema from '../schema/auth';
+
+const router = express.Router();
+
+export default router.use(
+  validator(schema.apiKey, ValidationSource.HEADER),
+  asyncHandler(async (req: PublicRequest, res, next) => {
+    req.apiKey = req.headers['x-api-key'].toString();
+
+    Logger.info(req.apiKey === apiKey);
+
+    if (apiKey) throw new ForbiddenError();
+    return next();
+  }),
+);

2d96dd421c23e2c702531655943278a73fa6567d
yu myeong cheol <mspringmspring@naver.com>
2020-10-17 16:06:42 +0900

    [CREATE] <model/Experience_report.ts>

---
+++ src/database/model/Experience_report.ts
@@ -0,0 +1,18 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { User } from './User';
+import { Experience } from './Experience';
+
+@Entity()
+export class Experience_report {
+  @PrimaryGeneratedColumn()
+  uuid: number;
+
+  @Column()
+  description: string;
+
+  @ManyToOne((type) => User, (user) => user.experience_report)
+  user!: User;
+
+  @ManyToOne((type) => Experience, (experience) => experience.experience_report)
+  experience!: Experience;
+}

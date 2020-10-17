ed30e8c3e11d1aff7d1c7af6e03a786208b5c19a
yu myeong cheol <mspringmspring@naver.com>
2020-10-17 16:00:48 +0900

    [CREATE] <auth/authentication.ts> token validation

---
+++ src/auth/authentication.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import schema from '../schema/auth';
+import { ProtectedRequest } from 'app-request';
+import validator, { ValidationSource } from '../middleware/validator';
+import asyncHandler from '../middleware/asyncHandler';
+import { getAccessToken, validateTokenData } from './authUtils';
+import UserRepo from '../database/repository/UserRepo';
+import { getCustomRepository } from 'typeorm';
+import JWT from '../core/JWT';
+import {
+  AuthFailureError,
+  AccessTokenError,
+  TokenExpiredError,
+} from '../core/apiError';
+
+const router = express.Router();
+
+export default router.use(
+  validator(schema.auth, ValidationSource.HEADER),
+  asyncHandler(async (req: ProtectedRequest, res, next) => {
+    req.accessToken = getAccessToken(req.headers.authorization);
+    const userRepo = getCustomRepository(UserRepo);
+
+    try {
+      const payload = await JWT.validate(req.accessToken);
+      validateTokenData(payload);
+
+      const user = await userRepo.findByUuid(payload.sub);
+      if (!user) throw new AuthFailureError('User not registered');
+      if (user.accessTokenKey !== payload.prm)
+        throw new AuthFailureError('Invalid access token');
+      req.user = user;
+
+      return next();
+    } catch (e) {
+      if (e instanceof TokenExpiredError) throw new AccessTokenError(e.message);
+      throw e;
+    }
+  }),
+);

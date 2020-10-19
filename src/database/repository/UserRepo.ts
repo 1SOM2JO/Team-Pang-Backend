ca3f70fe8a786e31c8c20aab96e8c19f9d4cd208
yu myeong cheol <mspringmspring@naver.com>
2020-10-19 23:14:18 +0900

    [ADD] <repository/UserRepo.ts>

--- src/database/repository/UserRepo.ts
+++ src/database/repository/UserRepo.ts
@@ -1,5 +1,6 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from '../model/User';
+import { promises } from 'dns';
 
 @EntityRepository(User)
 export default class UserRepository extends Repository<User> {
@@ -7,5 +8,33 @@ export default class UserRepository extends Repository<User> {
     return await this.createQueryBuilder('user')
       .where('user.uuid = :uuid', { uuid })
       .getOne();
+  }
+
+  public async findById(id: string): Promise<User> {
+    return await this.createQueryBuilder('user')
+      .where('user.id = :id', { id })
+      .getOne();
+  }
+
+  public async findByPhonenumber(phone: string): Promise<User> {
+    return await this.createQueryBuilder('user')
+      .where('user.phonenumber = :phonenumber', { phone })
+      .getOne();
+  }
+
+  public async createUser(
+    id: string,
+    permission: string,
+    nickname: string,
+    password: string,
+    phonenumber: string,
+  ): Promise<User> {
+    const user = new User();
+    user.id = id;
+    user.permission = permission;
+    user.nickname = nickname;
+    user.password = password;
+    user.phonenumber = phonenumber;
+    return await this.manager.save(user);
   }
 }

4dcd084aa0210a2182ecb1393acdcd23744223c5
yu myeong cheol <mspringmspring@naver.com>
2020-10-20 23:24:33 +0900

    [MODFIY]

--- src/app.ts
+++ src/app.ts
@@ -7,18 +7,19 @@ import 'reflect-metadata';
 import { NotFoundError, ApiError, InternalError } from './core/apiError';
 import { createConnection } from 'typeorm';
 import connectionOptions from './database';
-import redis from 'redis';
+import asyncRedis from 'async-redis';
 import morgan from 'morgan';
 import { port } from './config';
 import router from './routes';
 
-export const client = redis.createClient(6379, '127.0.0.1');
+export const client = asyncRedis.createClient(6379, '127.0.0.1');
 
 const app = express();
 
-app.listen(80, () => {
-  Logger.info(`server running on port : ${port}`);
-});
+if (environment === 'development')
+  app.listen(80, () => {
+    Logger.info(`server running on port : ${port}`);
+  });
 
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(
@@ -51,7 +52,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof ApiError) {
     ApiError.handle(err, res);
   } else {
-    if (environment === 'development') {
+    if (1) {
       Logger.error(err);
       return res.status(500).send(err.message);
     }

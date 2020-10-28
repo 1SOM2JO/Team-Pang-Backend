f91b9765e3f4415d7c94fc34f88d6721c2d5afc1
yu myeong cheol <mspringmspring@naver.com>
2020-10-28 16:51:14 +0900

    [MODIFY]

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 // import path from 'path';
-// import * as dotenv from 'dotenv';
+import { config } from 'dotenv';
 // dotenv.config({ path: path.join(__dirname, '../.env') });
+
+config();
 
 export const hostAddress = process.env.HOST_ADDRESS;
 export const environment = process.env.NODE_ENV;

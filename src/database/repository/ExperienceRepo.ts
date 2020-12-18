18f74ee3c3da94784fcf91f47da59997dbe8b6b2
mspring03 <mspringmspring@naver.com>
2020-12-18 19:04:45 +0900

    [ADD] </repository/ExperienceRepo.ts> findByUuid function

--- src/database/repository/ExperienceRepo.ts
+++ src/database/repository/ExperienceRepo.ts
@@ -1,8 +1,22 @@
 import { getConnection, getRepository } from 'typeorm';
 import { Request } from 'express';
 import { Experience } from '../model/Experience';
+import { expression } from '@hapi/joi';
+import { User } from '../model/User';
 
 class ExperienceRepository {
+    public static async findByUuid(uuid: number) {        
+        return await getRepository(Experience)
+            .createQueryBuilder('experience')
+            .leftJoinAndSelect('experience.user', 'user')
+            .where('experience.uuid = :uuid', { uuid })
+            .getOne();
+    }
+
+    // const today = new Date().toISOString().substring(0, 10);
+    // .where('experience.start_day <= :today', { today })
+    // .andWhere('experience.end_day >= :today', { today })
+
     public static async registration(
         experienceName: string,
         price: string,
@@ -12,6 +26,7 @@ class ExperienceRepository {
         startDay: string,
         endDay: string,
         image: string,
+        user: User
     ) {
         const experience = new Experience();
         experience.experience_name = experienceName;
@@ -19,9 +34,11 @@ class ExperienceRepository {
         experience.province = province;
         experience.county = county;
         experience.description = description;
+        experience.like = 0;
         experience.start_day = startDay;
         experience.end_day = endDay;
         experience.img = image;
+        experience.user = user;
         await getRepository(Experience).save(experience);
     }
 }

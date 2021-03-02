3f6979caf66335fa3b3b172ac824d6dc2d0eb251
mspring03 <mspringmspring@naver.com>
2021-03-02 19:42:49 +0900

    [MODIFY] <database/index.ts>

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,17 +1,25 @@
 import { ConnectionOptions } from 'typeorm';
 import { environment } from '../config';
 import { db } from '../config';
+import Container from 'typedi';
+import { Connection, createConnection, useContainer } from 'typeorm';
 
-const connectionOptions: ConnectionOptions = {
-  type: 'mysql',
-  synchronize: true,
-  logging: environment === 'development' ? true : false,
-  entities: ['./database/model/**/*.ts'],
-  database: db.name,
-  host: db.host,
-  port: db.port,
-  username: db.user,
-  password: db.password,
+export default async (): Promise<Connection> => {
+  const connectionOptions: ConnectionOptions = {
+    type: 'mysql',
+    synchronize: true,
+    logging: environment === 'development' ? true : false,
+    entities: ['./database/model/**/*.ts'],
+    database: db.name,
+    host: db.host,
+    port: db.port,
+    username: db.user,
+    password: db.password,
+  };
+
+  useContainer(Container);
+
+  const connection = await createConnection(connectionOptions);
+
+  return connection;
 };
-
-export default connectionOptions;
